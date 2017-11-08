d0bed15dccb5d913eb40c9cf8bb14d3fbcefc2db
Pierre Laporte <pierre@pingtimeout.fr>
2017-11-08 16:20:53 +0100

    Add bash debug colors

---
+++ .grc/conf.bash
@@ -0,0 +1,12 @@
+# Logs (TRACE, DEBUG, INFO, WARN, ERROR)
+
+# Sub commands
+regexp=^\+\+.*$
+colours="\033[38;5;153m"
+count=stop
+======
+# Commands
+regexp=^\+.*$
+colours="\033[38;5;84m"
+count=stop
+
