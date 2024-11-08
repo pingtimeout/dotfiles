0c6a46635fad61eab44fb6af78e6c39649db8d24
Pierre Laporte <pierre@pingtimeout.fr>
2024-11-08 10:38:01 +0100

    Display tabs and leading spaces for bash/heredoc/makefiles editing

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,9 @@ vim.opt.clipboard = ""
 -- In case you don't want to use `:LazyExtras`,
 -- then you need to set the option below.
 -- vim.g.lazyvim_picker = "fzf"
+vim.opt.list = true
+-- set listchars=tab:·┈,trail:￮,multispace:￮,lead:\ ,extends:▶,precedes:◀,nbsp:‿
+vim.opt.listchars = {
+  tab = ">-",
+  trail = "·",
+}
