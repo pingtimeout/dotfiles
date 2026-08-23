07e4838c5bb230f3e43962e4bdd7a17eb242612c
Pierre Laporte <pierre@pingtimeout.fr>
2026-08-23 22:09:38 +0200

    Update neovim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Do not use system clipboard by default
+vim.opt.clipboard = ""
+
+-- Enable list mode by default but only show tabs and trailing whitespaces
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = ">-",
+  trail = "·",
+}
